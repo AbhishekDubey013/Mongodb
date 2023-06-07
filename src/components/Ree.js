014c93e0a6012662de59de50771faaad91ac6339
Abhishek <abhishekdubey013@gmail.com>
2023-06-07 19:28:34 +0530

    hi

--- src/components/Ree.js
+++ src/components/Ree.js
@@ -1,3 +1,237 @@
+// import { useState, useEffect } from 'react';
+// import { useSelector } from 'react-redux';
+// import './App.css';
+// import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+// import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+// import { useDispatch } from 'react-redux';
+
+// const API_KEY = "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";
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const [messages, setMessages] = useState([
+//     {
+//       message: "",
+//       sentTime: "just now",
+//       sender: "ChatGPT"
+//     }
+//   ]);
+//   const [isTyping, setIsTyping] = useState(false);
+
+//   const questionResponses = useSelector(state => state.questionResponses);
+
+
+//   let data1 = useSelector((state)=>state.reducera.questionResponses);  
+
+//   const handleSend = async (message) => {
+//     const newMessage = {
+//       message,
+//       direction: 'outgoing',
+//       sender: "user"
+//     };
+
+//     const newMessages = [...messages, newMessage];
+    
+//     setMessages(newMessages);
+
+//     setIsTyping(true);
+//     await processMessageToChatGPT(newMessages);
+//   };
+
+//   const handleCompleteStoreData = async () => {
+//     setIsTyping(true);
+//     const storeData = data1.map(qr => ({
+//       message: `Q: ${qr.question}\nA: ${qr.response}`,
+//       sender: "user"
+//     }));
+
+//     const additionalMessage = {
+//       message:
+//         "Act as pssycology assistant: Give a very crisp inference and percentage on adhd response provided(not more than two lines), this data will be reviewed by a psyclogist further",
+//       sender: "user"
+//     };
+  
+//     await processMessageToChatGPT([...messages, ...storeData,additionalMessage]);
+
+//     dispatch({ type: 'RESET_QUESTION_RESPONSES' });
+//   };
+
+//   async function processMessageToChatGPT(chatMessages) {
+//     let apiMessages = chatMessages.map((messageObject) => {
+//       let role = "";
+//       if (messageObject.sender === "ChatGPT") {
+//         role = "assistant";
+//       } else {
+//         role = "user";
+//       }
+//       return { role: role, content: messageObject.message }
+//     });
+  
+//     const apiRequestBody = {
+//       "model": "gpt-3.5-turbo",
+//       "messages": [
+//         ...apiMessages
+//       ]
+//     }
+  
+//     await fetch("https://api.openai.com/v1/chat/completions", 
+//     {
+//       method: "POST",
+//       headers: {
+//         "Authorization": "Bearer " + API_KEY,
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(apiRequestBody)
+//     })
+//     .then((data) => data.json())
+//     .then((data) => {
+//       const responseMessage = data.choices[0].message.content;
+  
+//       setMessages([{
+//         message: responseMessage,
+//         sender: "ChatGPT"
+//       }]);
+//       setIsTyping(false);
+//     })
+//     .catch((error) => {
+//       console.error("Error processing message:", error);
+//       setIsTyping(false);
+//     });
+//   }
+
+//   // async function processMessageToChatGPT(chatMessages) {
+//   //   let apiMessages = chatMessages.map((messageObject) => {
+//   //     let role = "";
+//   //     if (messageObject.sender === "ChatGPT") {
+//   //       role = "assistant";
+//   //     } else {
+//   //       role = "user";
+//   //     }
+//   //     return { role: role, content: messageObject.message }
+//   //   });
+
+//   //   const apiRequestBody = {
+//   //     "model": "gpt-3.5-turbo",
+//   //     "messages": [
+//   //       ...apiMessages
+//   //     ]
+//   //   }
+
+//   //   await fetch("https://api.openai.com/v1/chat/completions", 
+//   //   {
+//   //     method: "POST",
+//   //     headers: {
+//   //       "Authorization": "Bearer " + API_KEY,
+//   //       "Content-Type": "application/json"
+//   //     },
+//   //     body: JSON.stringify(apiRequestBody)
+//   //   }).then((data) => {
+//   //     return data.json();
+//   //   }).then((data) => {
+//   //     setMessages([...chatMessages, {
+//   //       message: data.choices[0].message.content,
+//   //       sender: "ChatGPT"
+//   //     }]);
+//   //     setIsTyping(false);
+//   //   });
+//   // }
+
+//   return (
+//     // <div className="App">
+//     //   <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+//     //     <MainContainer>
+//     //       <ChatContainer>       
+//     //         <MessageList 
+//     //           scrollBehavior="smooth" 
+//     //           typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+//     //         >
+//     //           {messages.map((message, i) => {
+//     //             return <Message key={i} model={message} />
+//     //           })}
+//     //         </MessageList>
+//     //         <MessageInput placeholder="Type message here" onSend={handleSend} />        
+//     //       </ChatContainer>
+//     //     </MainContainer>
+//     //   </div>
+//     //   <div className="store-data">
+//     //     <h2>Store Data:</h2>
+//     //     {questionResponses && questionResponses.length > 0 ? (
+//     //       <ul>
+//     //         {questionResponses.map((qr, index) => (
+//     //           <li key={index}>
+//     //             <strong>Question:</strong> {qr.question}<br />
+//     //             <strong>Answer:</strong> {qr.answer}
+//     //           </li>
+//     //         ))}
+//     //       </ul>
+//     //     ) : (
+//     //       <p>No data available.</p>
+//     //     )}
+//     //     <button onClick={handleCompleteStoreData}>Send Complete Store Data to ChatGPT</button>
+//     //   </div>
+//     //   <div>
+//     //   <ul>
+//     //         {data1.map((qr, index) => (
+//     //           <li key={index}>
+//     //             <strong>Question:</strong> {qr.question}<br />
+//     //             <strong>Answer:</strong> {qr.response}
+//     //           </li>
+//     //         ))}
+//     //       </ul>
+//     //   </div>
+//     //   {console.log(data1)}
+//     // </div>
+
+//     <div className="App">
+//   <div className="chat-container">
+//     <div className="message-list">
+//       <MessageList scrollBehavior="smooth" typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}>
+//         {/* Comment out the MessageInput component */}
+//         {/* <MessageInput placeholder="Type message here" onSend={handleSend} /> */}
+//         {messages.map((message, i) => {
+//           return <Message key={i} model={message} />;
+//         })}
+//       </MessageList>
+//       {/* Add a center-align class to the message list */}
+//       <div className="center-align">
+//         <button className="btn btn-primary" onClick={handleSend}>Send</button>
+//       </div>
+//     </div>
+//     <div className="store-data">
+//       <h2>Store Data:</h2>
+//       {questionResponses && questionResponses.length > 0 ? (
+//         <ul>
+//           {questionResponses.map((qr, index) => (
+//             <li key={index}>
+//               <strong>Question:</strong> {qr.question}<br />
+//               <strong>Answer:</strong> {qr.answer}
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>No data available.</p>
+//       )}
+//       <button onClick={handleCompleteStoreData}>Send Complete Store Data to ChatGPT</button>
+//     </div>
+//   </div>
+//   <div className="data-list">
+//     <ul>
+//       {data1.map((qr, index) => (
+//         <li key={index}>
+//           <strong>Question:</strong> {qr.question}<br />
+//           <strong>Answer:</strong> {qr.response}
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+//   {console.log(data1)}
+// </div>
+
+//   );
+// }
+
+// export default App;
+
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
@@ -5,7 +239,7 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { useDispatch } from 'react-redux';
 
-const API_KEY = "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";
+const API_KEY = "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";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,8 +254,7 @@ function App() {
 
   const questionResponses = useSelector(state => state.questionResponses);
 
-
-  let data1 = useSelector((state)=>state.reducera.questionResponses);  
+  let data1 = useSelector((state) => state.reducera.questionResponses);
 
   const handleSend = async (message) => {
     const newMessage = {
@@ -31,7 +264,7 @@ function App() {
     };
 
     const newMessages = [...messages, newMessage];
-    
+
     setMessages(newMessages);
 
     setIsTyping(true);
@@ -47,11 +280,11 @@ function App() {
 
     const additionalMessage = {
       message:
-        "Act as pssycology assistant: Give a very crisp inference and percentage on adhd response provided(not more than two lines), this data will be reviewed by a psyclogist further",
+        "Act as psychology assistant: Give a very crisp inference and percentage on ADHD response provided (not more than two lines), this data will be reviewed by a psychologist further",
       sender: "user"
     };
-  
-    await processMessageToChatGPT([...messages, ...storeData,additionalMessage]);
+
+    await processMessageToChatGPT([...messages, ...storeData, additionalMessage]);
 
     dispatch({ type: 'RESET_QUESTION_RESPONSES' });
   };
@@ -66,167 +299,85 @@ function App() {
       }
       return { role: role, content: messageObject.message }
     });
-  
+
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
         ...apiMessages
       ]
-    }
-  
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    })
-    .then((data) => data.json())
-    .then((data) => {
-      const responseMessage = data.choices[0].message.content;
-  
-      setMessages([{
-        message: responseMessage,
-        sender: "ChatGPT"
-      }]);
-      setIsTyping(false);
-    })
-    .catch((error) => {
-      console.error("Error processing message:", error);
-      setIsTyping(false);
-    });
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      })
+      .then((data) => data.json())
+      .then((data) => {
+        const responseMessage = data.choices[0].message.content;
+
+        setMessages([{
+          message: responseMessage,
+          sender: "ChatGPT"
+        }]);
+        setIsTyping(false);
+      })
+      .catch((error) => {
+        console.error("Error processing message:", error);
+        setIsTyping(false);
+      });
   }
 
-  // async function processMessageToChatGPT(chatMessages) {
-  //   let apiMessages = chatMessages.map((messageObject) => {
-  //     let role = "";
-  //     if (messageObject.sender === "ChatGPT") {
-  //       role = "assistant";
-  //     } else {
-  //       role = "user";
-  //     }
-  //     return { role: role, content: messageObject.message }
-  //   });
-
-  //   const apiRequestBody = {
-  //     "model": "gpt-3.5-turbo",
-  //     "messages": [
-  //       ...apiMessages
-  //     ]
-  //   }
-
-  //   await fetch("https://api.openai.com/v1/chat/completions", 
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Authorization": "Bearer " + API_KEY,
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: JSON.stringify(apiRequestBody)
-  //   }).then((data) => {
-  //     return data.json();
-  //   }).then((data) => {
-  //     setMessages([...chatMessages, {
-  //       message: data.choices[0].message.content,
-  //       sender: "ChatGPT"
-  //     }]);
-  //     setIsTyping(false);
-  //   });
-  // }
+  useEffect(() => {
+    console.log("questionResponses:", questionResponses);
+    console.log("data1:", data1);
+  }, [questionResponses, data1]);
 
   return (
-    // <div className="App">
-    //   <div style={{ position:"relative", height: "800px", width: "700px"  }}>
-    //     <MainContainer>
-    //       <ChatContainer>       
-    //         <MessageList 
-    //           scrollBehavior="smooth" 
-    //           typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
-    //         >
-    //           {messages.map((message, i) => {
-    //             return <Message key={i} model={message} />
-    //           })}
-    //         </MessageList>
-    //         <MessageInput placeholder="Type message here" onSend={handleSend} />        
-    //       </ChatContainer>
-    //     </MainContainer>
-    //   </div>
-    //   <div className="store-data">
-    //     <h2>Store Data:</h2>
-    //     {questionResponses && questionResponses.length > 0 ? (
-    //       <ul>
-    //         {questionResponses.map((qr, index) => (
-    //           <li key={index}>
-    //             <strong>Question:</strong> {qr.question}<br />
-    //             <strong>Answer:</strong> {qr.answer}
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     ) : (
-    //       <p>No data available.</p>
-    //     )}
-    //     <button onClick={handleCompleteStoreData}>Send Complete Store Data to ChatGPT</button>
-    //   </div>
-    //   <div>
-    //   <ul>
-    //         {data1.map((qr, index) => (
-    //           <li key={index}>
-    //             <strong>Question:</strong> {qr.question}<br />
-    //             <strong>Answer:</strong> {qr.response}
-    //           </li>
-    //         ))}
-    //       </ul>
-    //   </div>
-    //   {console.log(data1)}
-    // </div>
-
     <div className="App">
-  <div className="chat-container">
-    <div className="message-list">
-      <MessageList scrollBehavior="smooth" typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}>
-        {/* Comment out the MessageInput component */}
-        {/* <MessageInput placeholder="Type message here" onSend={handleSend} /> */}
-        {messages.map((message, i) => {
-          return <Message key={i} model={message} />;
-        })}
-      </MessageList>
-      {/* Add a center-align class to the message list */}
-      <div className="center-align">
-        <button className="btn btn-primary" onClick={handleSend}>Send</button>
+      <div className="chat-container">
+        <div className="message-list">
+          <MessageList scrollBehavior="smooth" typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}>
+            {messages.map((message, i) => {
+              return <Message key={i} model={message} />;
+            })}
+          </MessageList>
+          <div className="center-align">
+            <button className="btn btn-primary" onClick={handleSend}>Send</button>
+          </div>
+        </div>
+        <div className="store-data">
+          <h2>Store Data:</h2>
+          {questionResponses && questionResponses.length > 0 ? (
+            <ul>
+              {questionResponses.map((qr, index) => (
+                <li key={index}>
+                  <strong>Question:</strong> {qr.question}<br />
+                  <strong>Answer:</strong> {qr.answer}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No data available.</p>
+          )}
+          <button onClick={handleCompleteStoreData}>Send Complete Store Data to ChatGPT</button>
+        </div>
       </div>
-    </div>
-    <div className="store-data">
-      <h2>Store Data:</h2>
-      {questionResponses && questionResponses.length > 0 ? (
+      <div className="data-list">
         <ul>
-          {questionResponses.map((qr, index) => (
+          {data1.map((qr, index) => (
             <li key={index}>
               <strong>Question:</strong> {qr.question}<br />
-              <strong>Answer:</strong> {qr.answer}
+              <strong>Answer:</strong> {qr.response}
             </li>
           ))}
         </ul>
-      ) : (
-        <p>No data available.</p>
-      )}
-      <button onClick={handleCompleteStoreData}>Send Complete Store Data to ChatGPT</button>
+      </div>
     </div>
-  </div>
-  <div className="data-list">
-    <ul>
-      {data1.map((qr, index) => (
-        <li key={index}>
-          <strong>Question:</strong> {qr.question}<br />
-          <strong>Answer:</strong> {qr.response}
-        </li>
-      ))}
-    </ul>
-  </div>
-  {console.log(data1)}
-</div>
-
   );
 }
 
